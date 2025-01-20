8adb164cc13ddab8f07a356ab859ccb2fcbaed1b
Oleksandr Malakhov <71847145+hisio13@users.noreply.github.com>
2025-01-20 19:56:48 +0100

    add index file

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.login-form');
 
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const email = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
+
+    if (!email || !password) {
+      alert('All form fields must be filled in');
+    }
+
+    const formData = {
+      email: email,
+      password: password
+    };
+
+    console.log(formData);
+
+    form.reset();
+  });
+});
